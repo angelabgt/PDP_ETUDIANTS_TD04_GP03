cef8c5783f7698ac30f10b450c768e8e67709d49
angelabgt <angela.bigot@edu.ece.fr>
2023-01-10 10:28:08 +0100

    etape_2

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,9 +1,63 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+
+
+
+
+// Copyleft 2020
+ 
+#include "DHT.h"
+ 
+ 
+// Definit le type de capteur utilise
+#define DHTTYPE DHT11
+ 
+// Declare un objet de type DHT
+// Il faut passer en parametre du constructeur 
+// de l'objet la broche et le type de capteur
+DHT_Unified dht(SENSOR, DHTTYPE);
+ 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+   
+  // Initialise la capteur DHT11
+  dht.begin();
 }
-
+ 
 void loop() {
-  // put your code here to run repeatedly
+  // Recupere la temperature et l'humidite du capteur et l'affiche
+  // sur le moniteur serie
+  sensors_event_t event;
+
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+
+  
+  
+ 
+  // Attend 10 secondes avant de reboucler
+  delay(10000);
 }
